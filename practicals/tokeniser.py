a0545c142e7a93fb3c77f26025c43e9646ae3487
jinhqian <89437043+jinhqian@users.noreply.github.com>
2021-10-04 13:43:36 -0400

    Add files via upload

---
+++ practicals/tokeniser.py
@@ -0,0 +1,19 @@
+import sys
+
+index = 1
+
+line = sys.stdin.readline()
+
+#sys.stdout.write(line)
+
+while line: 
+    #print(line)
+    print('# sent_id = %d' % (index))
+    index = index + 1
+    print('# text = %s' % (line)) 
+    tokens = line.replace(',', ' ,').replace('.', ' . ').replace("'", " ' ").replace(':', ' :').replace('and', ' and ').replace('(', ' (').replace(')', ' )').replace('"', ' " ').replace('  ', ' ').split()
+    token_id = 1
+    for token in tokens:
+        print('%d\t%s\t_\t_\t_\t_\t_\t_\t_\t_' % (token_id,token))
+        token_id = token_id + 1
+    line = sys.stdin.readline()
