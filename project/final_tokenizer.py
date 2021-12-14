551fb068846290058cb73e42ae2dbbcb345cd3c3
jinhqian <89437043+jinhqian@users.noreply.github.com>
2021-12-13 20:01:20 -0500

    Update and rename tokeniser.py to final_tokenizer.py

---
+++ project/final_tokenizer.py
@@ -0,0 +1,17 @@
+import sys
+
+index = 1
+
+line = sys.stdin.readline()
+
+while line:
+    print('# sent_id = %d' % (index))
+    index = index + 1
+    print('# text = %s' % (line.strip()))
+    tokens = line.replace('%', ' % ').replace('”', ' ” ').replace('“', ' “ ').replace('·', ' · ').replace(',', ' ,').replace('.', ' . ').replace("'", " ' ").replace(':', ' :').replace('~', ' ~ ').replace('(', ' ( ').replace(')', ' )').replace('"', ' " ').replace('에', ' 에').replace('년', ' 년').replace('은', ' 은').replace('는', ' 는').replace('으로',' 으로').replace('에서', ' 에서').replace('이다', ' 이다').replace('한다', ' 한다').replace('를', ' 를').replace('을', ' 을').replace('의', ' 의').replace(' \n', '\n').replace('\n ', '\n').replace('부터', ' 부터').replace('하고', ' 하고').split()
+    token_id = 1
+    for token in tokens:
+        print('%d\t%s\t_\t_\t_\t_\t_\t_\t_\t_' % (token_id,token))
+        token_id = token_id + 1
+    print()
+    line = sys.stdin.readline()
